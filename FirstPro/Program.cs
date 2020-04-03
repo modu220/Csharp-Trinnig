88ed8771686d269b631c58aa430ce7d676870b19
Mohamed Goleeb <m_goleeb@hotmail.com>
2020-04-03 22:24:39 +0300

    Add Commend

---
+++ FirstPro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mohamed Abdulmunim Ibrahim Goleeb");
+
+        }
+    }
+}
